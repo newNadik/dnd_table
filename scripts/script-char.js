da11f22755a5613554787cf5845d9c6fde46f7d2
VIVO\Vivo <i.am.nadik@gmail.com>
2018-06-28 16:39:35 +0300

    map

--- scripts/script-char.js
+++ scripts/script-char.js
@@ -1,67 +1,71 @@
 var charDropArea;
 
 function onStart() {
+  initChar()
+  initMap()
+}
+
+function initChar(){
   charDropArea = document.getElementById('char-drop-area');
 
   ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-    charDropArea.addEventListener(eventName, preventDefaults, false)
-    document.body.addEventListener(eventName, preventDefaults, false)
+    charDropArea.addEventListener(eventName, preventCharDefaults, false)
+    document.body.addEventListener(eventName, preventCharDefaults, false)
   });
 
   ['dragenter', 'dragover'].forEach(eventName => {
-    charDropArea.addEventListener(eventName, highlight, false)
+    charDropArea.addEventListener(eventName, highlightChar, false)
   });
 
   ['dragleave', 'drop'].forEach(eventName => {
-    charDropArea.addEventListener(eventName, unhighlight, false)
+    charDropArea.addEventListener(eventName, unhighlightChar, false)
   })
 
-  charDropArea.addEventListener('drop', handleDrop, false)
+  charDropArea.addEventListener('drop', handleCharDrop, false)
 }
 
-function preventDefaults (e) {
+function preventCharDefaults (e) {
   e.preventDefault()
   e.stopPropagation()
 }
 
-function highlight(e) {
+function highlightChar(e) {
   charDropArea.style.borderColor = 'gray';
 }
 
-function unhighlight(e) {
+function unhighlightChar(e) {
   charDropArea.style.borderColor = 'white';
 }
 
-function handleDrop(e) {
+function handleCharDrop(e) {
   var dt = e.dataTransfer
   var files = dt.files
 
-  handleFiles(files)
+  handleCharFiles(files)
 }
 
-function handleFiles(files) {
+function handleCharFiles(files) {
   files = [...files]
-  files.forEach(uploadFile)
-  files.forEach(previewFile)
+  files.forEach(uploadCharFile)
+  files.forEach(previewCharFile)
 }
 
-function previewFile(file) {
+function previewCharFile(file) {
   let reader = new FileReader()
   reader.readAsDataURL(file)
   reader.onloadend = function() {
     let img = document.createElement('img')
     img.setAttribute('width', '50px');
-    img.setAttribute('height', '50px');
     img.style.marginTop = '15px';
     img.style.marginLeft = '5px';
     img.style.marginRight = '5px';
     img.src = reader.result
     document.getElementById('gallery').appendChild(img)
-    document.getElementById('dropLabel').style.color = 'rgba(0,0,0,0)';
+    document.getElementById('dropCharLabel').style.color = 'rgba(0,0,0,0)';
   }
 }
 
-function uploadFile(file, i) {
+function uploadCharFile(file, i) {
   var url = 'https://api.cloudinary.com/v1_1/joezim007/image/upload'
   var xhr = new XMLHttpRequest()
   var formData = new FormData()
--- scripts/script-map.js
+++ scripts/script-map.js
@@ -1,4 +1,114 @@
+var mapDropArea,
+    scale = 1,
+    mapArea = document.getElementById('map-area'),
+    mapImage = document.getElementById('map-img');
 
-function onStart() {
+function initMap() {
 
+  mapDropArea = document.getElementById('map-drop-area');
+  ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    mapDropArea.addEventListener(eventName, preventMapDefaults, false)
+    document.body.addEventListener(eventName, preventMapDefaults, false)
+  });
+
+  ['dragenter', 'dragover'].forEach(eventName => {
+    mapDropArea.addEventListener(eventName, highlightMap, false)
+  });
+
+  ['dragleave', 'drop'].forEach(eventName => {
+    mapDropArea.addEventListener(eventName, unhighlightMap, false)
+  })
+
+  mapDropArea.addEventListener('drop', handleMapDrop, false)
+
+  document.getElementById('plus-button').onclick = function() { zoomIn()() };
+  document.getElementById('minus-button').onclick = function() { zoomOut() };
+}
+
+function preventMapDefaults (e) {
+  e.preventDefault()
+  e.stopPropagation()
+}
+
+function highlightMap(e) {
+  mapDropArea.style.borderColor = 'gray';
+}
+
+function unhighlightMap(e) {
+  mapDropArea.style.borderColor = 'white';
+}
+
+function handleMapDrop(e) {
+  var dt = e.dataTransfer
+  var files = dt.files
+
+  handleMapFile(files)
+}
+
+function handleMapFile(files) {
+  files = [...files]
+  files.forEach(uploadMapFile)
+  files.forEach(previewMapFile)
+}
+
+function previewMapFile(file) {
+  let reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onloadend = function() {
+    mapImage = document.createElement('img')
+    mapImage.setAttribute('id', 'map-img')
+    mapArea = document.getElementById('map-area')
+    mapImage.src = reader.result
+    mapImage.onload = function() {
+      if(mapImage.width - mapArea.width > mapImage.height - mapArea.height){
+        mapImage.setAttribute('width', '100%');
+      } else {
+        mapImage.setAttribute('height', '100%');
+      }
+      mapArea.appendChild(mapImage)
+      document.getElementById('map-drop-area').style.display = 'none';
+    };
+  }
+}
+
+function addPinchtoMap(){
+
+  interact(mapArea)
+  .gesturable({
+    onstart: function (event) { },
+    onmove: function (event) {
+      scale = scale * (1 + event.ds);
+
+      mapImage.style.webkitTransform =
+      mapImage.style.transform = 'scale(' + scale + ')';
+      dragMoveListener(event);
+    },
+    onend: function (event) { }
+  })
+  .draggable({ onmove: dragMoveListener });
+}
+
+function uploadMapFile(file, i) {
+  var url = 'https://api.cloudinary.com/v1_1/joezim007/image/upload'
+  var xhr = new XMLHttpRequest()
+  var formData = new FormData()
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+
+  formData.append('upload_preset', 'ujpu6gyk')
+  formData.append('file', file)
+  xhr.send(formData)
+}
+
+function zoomIn() {
+  scale = scale + 0.2;
+  mapImage.style.webkitTransform =
+    mapImage.style.transform = 'scale(' + scale + ')';
+}
+
+function zoomOut() {
+  scale = scale - 0.2;
+  if(scale < 1) scale = 1;
+  mapImage.style.webkitTransform =
+    mapImage.style.transform = 'scale(' + scale + ')';
 }
